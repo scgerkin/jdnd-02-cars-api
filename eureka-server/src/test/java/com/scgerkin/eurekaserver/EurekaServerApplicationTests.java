41f2dba32e80971d8ef2a5ba83c175d03e976780
Stephen Gerkin <stephen.gerkin@gmail.com>
2020-01-08 13:17:52 -0500

    Import Eureka Discovery Server

---
+++ eureka-server/src/test/java/com/scgerkin/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scgerkin.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
