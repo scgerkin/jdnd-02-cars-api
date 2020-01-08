96c2657c2ee37dc2f45e0c03f250d422c15b6b7c
Stephen Gerkin <stephen.gerkin@gmail.com>
2020-01-08 15:08:50 -0500

    Implement green test for PriceClient

---
+++ vehicles-api/src/test/java/com/udacity/vehicles/client/prices/PriceClientTest.java
@@ -0,0 +1,26 @@
+package com.udacity.vehicles.client.prices;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
+public class PriceClientTest {
+    @Autowired
+    private PriceClient priceClient;
+
+    @Test
+    public void searchOnIdReturnsExpectedResults() {
+        String priceString = priceClient.getPrice(1L);
+        Assert.assertEquals("USD 11018.84", priceString);
+    }
+
+}
