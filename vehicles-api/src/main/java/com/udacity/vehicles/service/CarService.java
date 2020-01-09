9b5fa1e42328981aa103896904a757e2cf17db7e
Stephen Gerkin <stephen.gerkin@gmail.com>
2020-01-09 18:16:14 -0500

    Implement updating car condition and modified at fields.
    Additionally add unit test for updating a car.

--- vehicles-api/src/main/java/com/udacity/vehicles/service/CarService.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/service/CarService.java
@@ -7,6 +7,7 @@ import com.udacity.vehicles.domain.Location;
 import com.udacity.vehicles.domain.car.Car;
 import com.udacity.vehicles.domain.car.CarRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -68,6 +69,8 @@ public class CarService {
                        .map(carToBeUpdated -> {
                            carToBeUpdated.setDetails(car.getDetails());
                            carToBeUpdated.setLocation(car.getLocation());
+                           carToBeUpdated.setCondition(car.getCondition());
+                           carToBeUpdated.setModifiedAt(LocalDateTime.now());
                            return repository.save(carToBeUpdated);
                        }).orElseThrow(CarNotFoundException::new);
         }
--- vehicles-api/src/test/java/com/udacity/vehicles/api/CarControllerTest.java
+++ vehicles-api/src/test/java/com/udacity/vehicles/api/CarControllerTest.java
@@ -4,9 +4,7 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -103,6 +101,18 @@ public class CarControllerTest {
             .andExpect(jsonPath("_embedded.carList", hasSize(1)))
             .andExpect(jsonPath("_embedded.carList[0].id", is(1)))
             .andExpect(jsonPath("_embedded.carList[0].details.body", is("sedan")));
+    }
+
+    @Test
+    public void updateCar() throws Exception {
+        Car car = getCar();
+        car.setCondition(Condition.NEW);
+        mvc.perform(
+            put(new URI("/cars/1"))
+                .content(json.write(car).getJson())
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_JSON_UTF8))
+            .andExpect(status().isOk());
     }
 
     /**
